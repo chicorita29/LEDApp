65650b9ad9daaac39c6e02fe2dea96191891a219
jiyeonlee <j1y11429@gmail.com>
2022-05-08 18:14:26 +0900

    Initial Commit

---
+++ LEDApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LEDApp
+//
+//  Created by 이지연 on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // do something!
+    }
+
+
+}
+
